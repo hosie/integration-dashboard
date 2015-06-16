4370a877b7d017fc58d436ac3c81f817a81a265a
hosie <jhosie@uk.ibm.com>
2015-06-16 12:09:10 +0100

    use real data in circle chart.js

--- src/public/Test/js/viewManualTestApp.js
+++ src/public/Test/js/viewManualTestApp.js
@@ -11,6 +11,7 @@
       var application112  = server11.addApplication("Application112");
       var application121  = server12.addApplication("Application121");
       var application122  = server12.addApplication("Application122");
+      var application123  = server12.addApplication("Application123");
       
       var flow1111        = application111.addMessageFlow("Flow1111");
       var flow1112        = application111.addMessageFlow("Flow1112");
@@ -21,6 +22,12 @@
       var flow1212        = application121.addMessageFlow("Flow1212");
       var flow1221        = application122.addMessageFlow("Flow1221");
       var flow1222        = application122.addMessageFlow("Flow1222");
+      
+      var flow1231        = application123.addMessageFlow("Flow1231");
+      var flow1232        = application123.addMessageFlow("Flow1232");
+      var flow1233        = application123.addMessageFlow("Flow1233");
+      
+      
       
       var node2           = integrationBusSimulation.addIntegrationNode("Node2");
       var server21        = node2.addIntegrationServer("Server21");
